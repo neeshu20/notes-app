074d71b800def9cfca946a06a5c37d8f2917702a
neeshu20 <125849680+neeshu20@users.noreply.github.com>
2024-04-19 10:12:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import uuid from "react-uuid";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
+
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+          <div
+            key={id}
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button onClick={(e) => onDeleteNote(id)}>Delete</button>
+            </div>
+
+            <p>{body && body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+              Last Modified{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (field, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [field]: value,
+      lastModified: Date.now(),
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Active Note</div>;
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          placeholder="Note Title"
+          value={activeNote.title}
+          onChange={(e) => onEditField("title", e.target.value)}
+          autoFocus
+        />
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          value={activeNote.body}
+          onChange={(e) => onEditField("body", e.target.value)}
+        />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">
+          {activeNote.body}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+  };
+
+  const onDeleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArr);
+  };
+
+  const getActiveNote = () => {
+    return notes.find(({ id }) => id === activeNote);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+export default App;
+
